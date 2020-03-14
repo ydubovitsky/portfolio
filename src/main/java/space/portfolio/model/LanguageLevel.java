c4bc3fbd039fb46de1c14b9fc8f0544b4ea25ef1
y.dubovitsky <y.dubovitsky@gmail.com>
2020-03-14 13:54:09 +0300

    Created and filled the database. Borrowed a data generator for the database.

---
+++ src/main/java/space/portfolio/model/LanguageLevel.java
@@ -0,0 +1,27 @@
+package space.portfolio.model;
+
+/**
+ * 
+ * @author devstudy
+ * @see http://devstudy.net
+ */
+public enum LanguageLevel {
+
+	BEGINNER,
+	
+	ELEMENTARY,
+	
+	PRE_INTERMEDIATE,
+	
+	INTERMEDIATE,
+	
+	UPPER_INTERMEDIATE,
+	
+	ADVANCED,
+	
+	PROFICIENCY;
+	
+	public String getDbValue(){
+		return name().toLowerCase();
+	}
+}
